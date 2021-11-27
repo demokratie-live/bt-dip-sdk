9f97fee8977161f983a77e97bbfbce2936b38dae
ManAnRuck <github@manuelruck.de>
2021-11-27 15:14:36 +0100

    fix response type in test
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- src/tests/usage.ts
+++ src/tests/usage.ts
@@ -7,19 +7,19 @@ const config = new Configuration({
 
 const api = new DipApi(config);
 
-let cursor: string | undefined = undefined;
+let cursor: any = undefined; // Issue: Why this should be any? If it's of type "string | undefined" response type is any. :'(
 
 (async () => {
   let hasNext: boolean = true;
   let counter = 0;
   while (hasNext) {
-    const { data } = (await api.getVorgaenge({
+    const { data } = await api.getPersonen({
       cursor,
-    })) as any;
+    });
     hasNext = cursor !== data.cursor;
     cursor = data.cursor;
-    counter += data.documents.length;
-
+    counter += data.documents?.length ?? 0;
+    console.log(data.documents?.[0].vorname)
     console.log(
       `-${data.cursor}- ${counter}/${(data as VorgangResponse).numFound}`
     );
