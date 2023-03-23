4274e2edc8837f1a57add75513f3411afe7c584a
Manuel Ruck <git@manuelruck.de>
2023-03-23 22:33:31 +0100

    upgrade swagger
    - use latest swagger yaml
    - generate script use latest swagger
    - add axios rate limiter
    - remove doku
    
    Signed-off-by: Manuel Ruck <git@manuelruck.de>

--- src/tests/usage.ts
+++ src/tests/usage.ts
@@ -1,27 +1,36 @@
-import { DipApi, Configuration } from "../index";
-import { VorgangResponse } from "../__generated";
+import {
+  AktivittenApi,
+  AktivittenApiGetAktivitaetListRequest,
+  Configuration,
+} from "../index";
+import rateLimitedInstance from "./axios";
 
+// Create a new Configuration instance with the provided API key
 const config = new Configuration({
   apiKey: `ApiKey ${process.env.API_KEY}`,
 });
 
-const api = new DipApi(config);
+// Create a new AktivittenApi instance using the provided configuration
+// and rateLimitedInstance (which limits the requests to one every 3 seconds)
+const api = new AktivittenApi(config, undefined, rateLimitedInstance);
 
-let cursor: any = undefined; // Issue: Why this should be any? If it's of type "string | undefined" response type is any. :'(
-
+// Define an asynchronous function to fetch activity lists
 (async () => {
+  // Initialize the cursor with the appropriate type
+  let cursor: AktivittenApiGetAktivitaetListRequest["cursor"] = undefined;
   let hasNext: boolean = true;
   let counter = 0;
+  
+    // While there are more activities to fetch
   while (hasNext) {
-    const { data } = await api.getAktivitaeten({
+    // Request the activity list using the current cursor
+    const { data } = await api.getAktivitaetList({
       cursor,
     });
     hasNext = cursor !== data.cursor;
     cursor = data.cursor;
     counter += data.documents?.length ?? 0;
 
-    console.log(
-      `-${data.cursor}- ${counter}/${(data as VorgangResponse).numFound}`
-    );
+    console.log(`-${data.cursor}- ${counter}/${data.numFound}`);
   }
 })();
